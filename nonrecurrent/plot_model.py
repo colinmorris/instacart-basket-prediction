5c027e112299e1978abc629ddca3327b858a10e2
colinmorris <colin.morris2@gmail.com>
2017-07-25 07:21:13 -0400

    plotting stuff

--- nonrecurrent/plot_model.py
+++ nonrecurrent/plot_model.py
@@ -6,15 +6,23 @@ from matplotlib import pyplot as plt
 
 from baskets import common
 
+from dataset import Dataset
+
 parser = argparse.ArgumentParser()
 parser.add_argument('tag')
 args = parser.parse_args()
 path = common.resolve_xgboostmodel_path(args.tag)
 model = xgb.Booster(model_file=path)
 
-def plot_importance():
-  fig, ax = plt.subplots()
-  xgb.plot_importance(model, ax)
+def patch_featnames():
+  # this is some riggery and tomfoolery
+  model.feature_names = Dataset.feat_cols
+  model.feature_types = None
+
+patch_featnames()
+
+def plot_importance(importance_type='weight'):
+  xgb.plot_importance(model, importance_type=importance_type)
 
 
 def plot_trees(n=2):
