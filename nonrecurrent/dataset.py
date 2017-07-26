b009872304f965fd0041c5e18a0ecc6a80a11853
colinmorris <colin.morris2@gmail.com>
2017-07-25 17:34:12 -0700

    weighting, custom fscore eval metric for xgb

--- nonrecurrent/dataset.py
+++ nonrecurrent/dataset.py
@@ -8,10 +8,10 @@ from scalar_vectorize import all_fields
 class Dataset(object):
   """Wrapper around the recarrays serialized by scalar_vectorize.py"""
 
-  non_feat_cols = {'label', 'weight', 'uid'}
+  non_feat_cols = {'label', 'weight', 'uid', 'orderid'}
   feat_cols = [col for col in all_fields if col not in non_feat_cols]
 
-  def __init__(self, tag, maxlen=None):
+  def __init__(self, tag, weight=False, maxlen=None):
     npy_path = common.resolve_scalarvector_path(tag)
     dat = np.load(npy_path)
     if maxlen:
@@ -20,6 +20,10 @@ class Dataset(object):
     featdat = dat[self.feat_cols]
     self.featdat = featdat.view(float).reshape(len(featdat), -1)
     self.labels = dat['label']
+    self.weight = weight
 
   def as_dmatrix(self):
-    return xgb.DMatrix(self.featdat, label=self.labels, feature_names=self.feat_cols)
+    kwargs = dict(label=self.labels, feature_names=self.feat_cols)
+    if self.weight:
+      kwargs['weight'] = self.records['weight']
+    return xgb.DMatrix(self.featdat, **kwargs)
--- nonrecurrent/train.py
+++ nonrecurrent/train.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
+from __future__ import division
 import argparse
 import xgboost as xgb
+from collections import defaultdict
 
 from baskets import common
 from baskets.time_me import time_me
@@ -23,18 +25,52 @@ xgb_params = {
     ,"lambda"           :10
 }
 
+P_THRESH = .2
+THRESH = (P_THRESH)
+
+counter = 0
 def train(traindat, params, tag, nrounds):
-  valdat = Dataset('validation')
+  valdat = Dataset('validation', weight=True)
   # TODO: try weight parameter
   # TODO: try set_base_margin (https://github.com/dmlc/xgboost/blob/master/demo/guide-python/boost_from_prediction.py)
   # TODO: early_stopping_rounds
   dtrain = traindat.as_dmatrix()
+  def quick_fscore(preds, _notused_dtrain):
+    global counter
+    counter += 1
+    if 1 and counter % 5 != 0:
+      return 'fscore', 0.0
+    with time_me('calculated validation fscore', mode='print'):
+      user_counts = defaultdict(lambda : dict(tpos=0, fpos=0, fneg=0))
+      for i, record in enumerate(valdat.records):
+        uid = record['uid']
+        pred = preds[i] >= THRESH
+        label = record['label']
+        if pred and label:
+          user_counts[uid]['tpos'] += 1
+        elif pred and not label:
+          user_counts[uid]['fpos'] += 1
+        elif label and not pred:
+          user_counts[uid]['fneg'] += 1
+      fscore_sum = 0
+      for uid, res in user_counts.iteritems():
+        numerator = 2 * res['tpos']
+        denom = numerator + res['fpos'] + res['fneg']
+        if denom == 0:
+          fscore = 1
+        else:
+          fscore = numerator / denom
+        fscore_sum += fscore
+      return 'fscore', fscore_sum / len(user_counts)
+    
   dval = valdat.as_dmatrix()
   # If you pass in more than one value to evals, early stopping uses the
   # last one. Because why not.
   watchlist = [(dtrain, 'train'), (dval, 'validation'),]
+  #watchlist = [(dval, 'validation'),]
 
-  model = xgb.train(xgb_params, dtrain, nrounds, evals=watchlist, early_stopping_rounds=10)
+  model = xgb.train(xgb_params, dtrain, nrounds, evals=watchlist, 
+      early_stopping_rounds=10) #, feval=quick_fscore, maximize=True)
   # Set output_margin=True to get logits
   model_path = common.resolve_xgboostmodel_path(tag)
   model.save_model(model_path)
@@ -48,9 +84,10 @@ def main():
       help='Number of rounds of boosting')
   parser.add_argument('--n-users', type=int, 
       help='Limit number of users tested on (default: no limit)')
+  parser.add_argument('--weight', action='store_true')
   args = parser.parse_args()
 
-  dataset = Dataset(args.train_recordfile, maxlen=args.n_users)
+  dataset = Dataset(args.train_recordfile, maxlen=args.n_users, weight=args.weight)
   params = xgb_params
   with time_me(mode='stderr'):
     train(dataset, params, args.tag, args.n_rounds)
