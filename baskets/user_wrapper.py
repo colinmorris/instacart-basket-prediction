84d48064335ad93a7b81afd9de68ad3ce595a329
colinmorris <colin.morris2@gmail.com>
2017-08-13 19:57:39 +0000

    clear out dusty old user_wrapper methods

--- baskets/user_wrapper.py
+++ baskets/user_wrapper.py
@@ -15,10 +15,9 @@ class UserWrapper(object):
   # (day, hour, days_since) + (prev_ordered, prev_nprods, prev_nreorders)
   NFEATS = 3 + 3 
 
-  def __init__(self, user, feat_fixture=None):
+  def __init__(self, user):
     self.user = user
     self._all_pids = None
-    self.feat_fixture = feat_fixture
 
   @property
   def uid(self):
@@ -100,89 +99,7 @@ class UserWrapper(object):
     iprod = random.randint(0, len(order.products)-1)
     return order.products[iprod]
 
-  # TODO: clear out dusty old methods
-
-  rawcols = ['dow', 'hour', 'days_since_prior',
-      'previously_ordered', 'n_prev_products', 'n_prev_repeats', 'n_prev_reorders']
-  def rawfeats_to_df(self, featdata):
-    return pd.DataFrame(featdata, columns=self.rawcols)
-  def transform_raw_feats(self, featdata, maxlen): 
-    df = self.rawfeats_to_df(featdata)
-    if self.feat_fixture:
-      feats, dimen = self.feat_fixture
-      return vectorize(df, self.user, maxlen, feats, dimen)
-    else:
-      return vectorize(df, self.user, maxlen)
-
-  def training_sequence_for_pid(self, pid, maxlen, product_df=None, testmode=False):
-    """Return a dict of (x, labels, seqlen, lossmask, pid)
-    Where x is an ndarray, seqlen and pid are scalars, and everything else is a 1-d array
-
-    In test mode, include the final order (which will have unknown label)
-    """
-    seqlen = self.seqlen
-    if testmode:
-      assert self.user.test
-      seqlen += 1
-    x = np.zeros([seqlen, len(self.rawcols)])
-    labels = np.zeros([maxlen])
-    lossmask = np.zeros([maxlen])
-    # Start from second order (because of reasons)
-    # This is pretty hacky. Make a copy of the current user, and add their 
-    # "testorder" to the end of their array of normal orders.
-    user = self.user
-    if testmode:
-      user = User()
-      user.CopyFrom(self.user)
-      lastorder = user.orders.add()
-      lastorder.CopyFrom(self.user.testorder)
-      assert len(user.orders) == self.norders + 1
-    prevs, orders = itertools.tee(user.orders)
-    orders.next()
-    seen_first = False
-    prevprev = None # The order before last
-    # ids of products seen up to prev order (but not including products in prev
-    # order itself)
-    pids_seen = set()
-    for i, prev, order in itertools.izip(range(maxlen), prevs, orders):
-      ordered = pid in order.products
-      prevprods_in_order = list(prev.products)
-      try:
-        # Order in which it was added to the cart (first=1)
-        previously_ordered = prevprods_in_order.index(pid) + 1
-      except ValueError:
-        previously_ordered = 0
-      labels[i] = int(ordered)
-      seen_first = seen_first or previously_ordered
-      # We only care about our ability to predict when a product is *re*ordered. 
-      # So we zero out the loss for predicting all labels up to and including the
-      # first order that has that product. (We also zero out the loss past the end
-      # of the actual sequence, i.e. for the padding section)
-      lossmask[i] = int(bool(seen_first))
-      prevprods = set(prev.products)
-      if prevprev is None:
-        prev_repeats = 0
-      else:
-        prod2 = set(prevprev.products)
-        prev_repeats = len(prevprods.intersection(prod2))
-      prev_reorders = len(prevprods.intersection(pids_seen))
-      x[i] = (
-          [order.dow, order.hour, order.days_since_prior]
-        + [previously_ordered, len(prev.products), prev_repeats, prev_reorders]
-      )
-      prevprev = prev
-      pids_seen.update(set(prev.products))
-
-    feats = self.transform_raw_feats(x, maxlen)
-    res = dict(x=feats, labels=labels, seqlen=seqlen, lossmask=lossmask,
-        pindex=pid-1, xraw=x
-    )
-    if product_df is not None:
-      aid, did = product_df.loc[pid-1, ['aisle_id', 'department_id']]
-      res['aisle_id'] = aid-1
-      res['dept_id'] = did-1
-    return res
-
+# deprecated
 def vectorize(df, user, maxlen, features=None, nfeats=None):
   features = features or FEATURES # Default to all of them
   nfeats = nfeats or NFEATS
